e5a7f1a13c3726c762b8517b8b215bfd979fe839
ShotaAk <macakasit@gmail.com>
2020-05-14 18:32:50 +0900

    Add changeState function

--- src/object_tracking_component.cpp
+++ src/object_tracking_component.cpp
@@ -21,11 +21,30 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "lifecycle_msgs/srv/change_state.hpp"
 
 using namespace std::chrono_literals;
 
 namespace object_tracking
 {
+
+template<typename FutureT, typename WaitTimeT>
+std::future_status
+wait_for_result(
+  FutureT & future,
+  WaitTimeT time_to_wait)
+{
+  auto end = std::chrono::steady_clock::now() + time_to_wait;
+  std::chrono::milliseconds wait_period(100);
+  std::future_status status = std::future_status::timeout;
+  do {
+    auto now = std::chrono::steady_clock::now();
+    auto time_left = end - now;
+    if (time_left <= std::chrono::seconds(0)) {break;}
+    status = future.wait_for((time_left < wait_period) ? time_left : wait_period);
+  } while (rclcpp::ok() && status != std::future_status::ready);
+  return status;
+}
 
 Talker::Talker(const rclcpp::NodeOptions & options)
 : Node("talker", options), count_(0)
@@ -35,6 +54,31 @@ Talker::Talker(const rclcpp::NodeOptions & options)
 
   // Use a timer to schedule periodic message publishing.
   timer_ = create_wall_timer(1s, std::bind(&Talker::on_timer, this));
+
+  auto client_change_state = create_client<
+    lifecycle_msgs::srv::ChangeState>("raspimouse/change_state");
+
+  if (!client_change_state->wait_for_service(3s)) {
+    RCLCPP_ERROR(get_logger(), "Service %s is not available.",
+      client_change_state->get_service_name());
+  }
+
+  auto request = std::make_shared<lifecycle_msgs::srv::ChangeState::Request>();
+  request->transition.id = lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE;
+
+  auto future_result = client_change_state->async_send_request(request);
+
+  auto future_status = wait_for_result(future_result, 3s);
+
+  if (future_status != std::future_status::ready) {
+    RCLCPP_ERROR(get_logger(), "Server time out");
+  } else {
+    if (future_result.get()) {
+      RCLCPP_INFO(get_logger(), "Change State result:%d", future_result.get()->success);
+    } else {
+      RCLCPP_ERROR(get_logger(), "Failt to change state");
+    }
+  }
 }
 
 void Talker::on_timer()
