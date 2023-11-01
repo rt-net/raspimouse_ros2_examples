572ae26fba3c0ee6448a577c6538c6b569e229b1
YusukeKato <yusuke.kato@rt-net.jp>
2023-11-01 15:44:28 +0900

    usb_camノードにもComposableNodeを使用

--- launch/object_tracking.launch.py
+++ launch/object_tracking.launch.py
@@ -55,6 +55,17 @@ def generate_launch_description():
                     name='raspimouse',
                     parameters=[{'use_light_sensors': False}],
                     condition=IfCondition(LaunchConfiguration('mouse'))),
+                ComposableNode(
+                    package='usb_cam',
+                    plugin='usb_cam::UsbCamNode',
+                    name='usb_cam',
+                    remappings=[('image_raw', 'camera/color/image_raw')],
+                    parameters=[
+                        {'video_device': LaunchConfiguration('video_device')},
+                        {'frame_id': 'camera_color_optical_frame'},
+                        {'pixel_format': 'yuyv2rgb'}
+                    ],
+                    condition=IfCondition(LaunchConfiguration('use_camera_node'))),
             ],
             output='screen',
     )
@@ -67,23 +78,10 @@ def generate_launch_description():
         parameters=[{'components': ['raspimouse', 'tracker']}]
     )
 
-    usb_cam_node = Node(
-        package='usb_cam',
-        executable='usb_cam_node_exe',
-        remappings=[('image_raw', 'camera/color/image_raw')],
-        parameters=[
-            {'video_device': LaunchConfiguration('video_device')},
-            {'frame_id': 'camera_color_optical_frame'},
-            {'pixel_format': 'yuyv2rgb'}
-        ],
-        condition=IfCondition(LaunchConfiguration('use_camera_node'))
-    )
-
     return launch.LaunchDescription([
         declare_mouse,
         declare_use_camera_node,
         declare_video_device,
         container,
-        manager,
-        usb_cam_node
+        manager
     ])
