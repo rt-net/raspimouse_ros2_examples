8098d9ed50fccbb4ef31fa2bcd1b3e9d6407cd4d
KuraZuzu <kurasawa@rt-net.jp>
2024-11-21 09:39:43 +0900

    Update Copyright years

--- raspimouse_ros2_examples/joystick_control.py
+++ raspimouse_ros2_examples/joystick_control.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: UTF-8
 
-# Copyright 2020 RT Corporation
+# Copyright 2020-2024 RT Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
