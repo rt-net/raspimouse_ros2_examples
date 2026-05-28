3a85b01eea6df5b249112b290a236db9bbda3b9e
YusukeKato <yusuke.kato@rt-net.jp>
2026-05-28 13:34:06 +0900

    不要なimportを削除

--- launch/line_follower.launch.py
+++ launch/line_follower.launch.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
