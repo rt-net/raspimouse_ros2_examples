449625e386f52bc5097b5e1ec46fec2d6778e5e7
YusukeKato <yusuke.kato@rt-net.jp>
2026-05-29 17:29:45 +0900

    importの順序を修正

--- launch/teleop_joy.launch.py
+++ launch/teleop_joy.launch.py
@@ -17,8 +17,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.events import Shutdown
 from launch.conditions import IfCondition
+from launch.events import Shutdown
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import LifecycleNode, Node
 
